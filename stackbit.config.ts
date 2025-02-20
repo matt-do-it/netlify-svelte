326a025442efe940adf9e40c429754357ded706b
Matt Herold <mattherold@icloud.com>
2025-02-20 22:28:34 +0100

    .

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -2,9 +2,11 @@ import { defineStackbitConfig } from '@stackbit/types';
 import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
+    "stackbitVersion": "~0.6.0",
     "nodeVersion": "18",
     "ssgName": "custom",
     "devCommand": "yarn run dev --port {PORT}",
+    "postInstallCommand": "npm i --no-save @stackbit/types",
   experimental: {
     ssg: {
       name: "sveltekit",
