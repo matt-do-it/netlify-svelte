34d4cfd2bfe62adc32e61ac91a0b10673cf058d8
Matt Herold <mattherold@icloud.com>
2025-02-20 21:34:55 +0100

    Added Slug and edit.

--- src/layouts/layout.svelte
+++ src/layouts/layout.svelte
@@ -1,7 +1,17 @@
+<script context="module" lang="ts">
+  // If you're using TypeScript, you can add types here
+  import { page } from '$app/stores';
+</script>
+
 <script>
+  // Accessing the slug from the route params
   export let title;
   export let subline;
+  export let slug = $page.route.id; 
 </script>
+
+<div data-sb-object-id="routes/{slug}/+page.md">
+
 
 <div class="ml-[4vmin] mr-[4vmin] md:mr-8 md:ml-8">
   <div class="container mx-auto pt-px">
@@ -13,13 +23,17 @@
       </div>
       <div class="lg:basis-1/2 lg:order-1">
         <article class="prose pt-16">
-          <h1 class="text-5xl mb-8">{title}</h1>
-          <p class="text-3xl mt-8">{subline}</p>
+          <h1 class="text-5xl mb-8" data-sb-field-path="title">{title}</h1>
+          <p class="text-3xl mt-8" data-sb-field-path="subline">{subline}</p>
+          <div data-sb-field-path="markdown_content">
           <slot>
             <!-- the mdsvex content will be slotted in here -->
           </slot>
+          </div>
         </article>
       </div>
     </div>
   </div>
+</div>
+
 </div>
