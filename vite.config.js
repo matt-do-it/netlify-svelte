fb64a7689ae7eff267c094bf8027b37063703adc
Matt Herold <mattherold@icloud.com>
2025-02-20 22:19:40 +0100

    Config.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  server: { hmr: { path: "/vite-hmr/" } },
   plugins: [sveltekit(), tailwindcss()],
 });
